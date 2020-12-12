4598ba4ae83add9094be392cd0982e972e29f33f
Davo <davidakinfenwa2@gmail.com>
2020-12-12 17:59:02 +0100

    Meta tags set properly and others.

--- static/assets/js/custom-js.js
+++ static/assets/js/custom-js.js
@@ -1,3 +1,9 @@
+var current_theme = document.cookie.replace("theme=","")
+if(current_theme=="dark"){
+    document.getElementById("main-list").className = "all-websites-main-list-dark row";
+    document.getElementById("theme-cont").style.backgroundColor = "black";
+    document.getElementById("theme-button").className = "theme-button-dark";
+}
 function changeTheme() {
 	var mainlist = document.getElementById("main-list");
 	var button = document.getElementById("theme-button");
@@ -7,32 +13,12 @@ function changeTheme() {
 		mainlist.className = "all-websites-main-list-dark row";
 		button.className = "theme-button-dark";
 		cont.style.backgroundColor = "black";
+        document.cookie = "theme=dark; path=/;";
 	} else if (mainlist.className == "all-websites-main-list-dark row") {
 		mainlist.className = "all-websites-main-list row";
 		button.className = "theme-button-light";
 		cont.style.backgroundColor = "white";
+        document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 	}
 }
-
-function changeTheme_ordered() {
-
-	var mainorderedlist = document.getElementById("main-ordered-list");
-	var button = document.getElementById("theme-button");
-	if (mainorderedlist.className == "all-websites-main-list row") {
-		mainorderedlist.className = "all-websites-main-list-dark row";
-		button.className = "theme-button-dark";
-
-	} else if (mainorderedlist.className == "all-websites-main-list-dark row") {
-		mainorderedlist.className = "all-websites-main-list row";
-		button.className = "theme-button-light";
-	}
-
-}
-
-
-
-
-
-
-
 
